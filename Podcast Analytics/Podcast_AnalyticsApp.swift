6cfce3e4a11cb51a63a0d9ae7fe418aea27096f3
Connor Norton <connorn314@gmail.com>
2023-02-22 13:11:34 -0800

    Initial Commit

---
+++ Podcast Analytics/Podcast_AnalyticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Podcast_AnalyticsApp.swift
+//  Podcast Analytics
+//
+//  Created by Connor Norton on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Podcast_AnalyticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
